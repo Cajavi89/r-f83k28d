b04756b2cd99a59a8a0d263c71f849b9de733eb6
Carlos <cajavi89@gmail.com>
2021-11-29 18:32:13 -0500

    solucion de test

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+constructor(){
+  super();
+  this.state= {
+    value:0
+  }
+  this.incrementar = this.incrementar.bind(this)
+
+}
+  incrementar(){
+    this.setState({
+      value: this.state.value +1
+    })
+  }
+
   render() {
+
+
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.value}</span>
+        <button id="inc" onClick={this.incrementar}>Incrementa</button>
       </div>
     );
   }
